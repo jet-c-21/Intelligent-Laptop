177fc52b0bf6f5e563f5bbb6136ecd46613aa902
Jet Chien <67193495+jet-chien@users.noreply.github.com>
2020-11-28 23:18:01 +0800

    add url to img

--- face_ult/img_tool.py
+++ face_ult/img_tool.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from urllib.request import urlopen
 
 
 class ImgTool:
@@ -7,3 +8,12 @@ class ImgTool:
     def get_clarity_val(img: np.ndarray) -> int:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         return int(cv2.Laplacian(gray, cv2.CV_64F).var())
+
+    @staticmethod
+    def url_to_img(url: str) -> np.ndarray:
+        try:
+            response = urlopen(url)
+            image = np.asarray(bytearray(response.read()), dtype="uint8")
+            return cv2.imdecode(image, cv2.IMREAD_COLOR)
+        except Exception as e:
+            print(f"failed to convert url to image. url: {url}  Error: {e}")
